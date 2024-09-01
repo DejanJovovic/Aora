0bf91b79901a07c9e8f518030e6986ab9e15ee67
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2024-09-01 12:11:16 +0200

    set up appwrite cloud

---
+++ AoraVideos/lib/appwrite.js
@@ -0,0 +1,9 @@
+export const appwriteConfig = {
+  endpoint: "https://cloud.appwrite.io/v1",
+  platfrom: "com.deksi.aora",
+  projectId: "66d43a12002bc2ef66aa",
+  databaseId: "66d43b530017940fd1af",
+  userCollectionId: "66d43b84002889f58f92",
+  videoCollectionId: "66d43ba0002834bd3f11",
+  storageId: "66d43d3f000d6ac45e4d",
+};
